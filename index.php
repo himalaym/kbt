9cbb18b8e1001b530e621a9911b7216f091689a0
himalaym <himalaym@thewebfosters.com>
2016-09-13 11:19:34 +0530

    my first commit

---
+++ index.php
@@ -0,0 +1,375 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+		<title>Kamdhenu Borewell and Trading</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+		<link rel='stylesheet' id='camera-css'  href='css/camera.css' type='text/css' media='all'>
+
+		<link rel="stylesheet" type="text/css" href="css/slicknav.css">
+		<link rel="stylesheet" href="css/prettyPhoto.css" type="text/css" media="screen" title="prettyPhoto main stylesheet" charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="uploadify/uploadify.css">
+		<link href="css/toastr.css" rel="stylesheet"/>
+		
+		
+		<script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
+
+		<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700|Open+Sans:700' rel='stylesheet' type='text/css'>
+		<script type="text/javascript" src="js/jquery.mobile.customized.min.js"></script>
+		<script type="text/javascript" src="js/jquery.easing.1.3.js"></script> 
+		<script type="text/javascript" src="js/camera.min.js"></script>
+		<script type="text/javascript" src="js/myscript.js"></script>
+		<script src="js/sorting.js" type="text/javascript"></script>
+		<script src="js/jquery.isotope.js" type="text/javascript"></script>
+		<script src="js/toastr.js"></script>
+		<script type="text/javascript" src="uploadify/jquery.uploadify.min.js"></script>
+		<!--script type="text/javascript" src="js/jquery.nav.js"></script-->	
+	</head>
+	<body>
+    
+    <!--home start-->
+    
+    <div id="home">
+    	<div class="headerLine">
+	<div id="menuF" class="default">
+		<div class="container">
+			<div class="row">
+				<div class="logo col-md-4">
+					<div>
+						<h2>KBT</h2>
+					</div>
+				</div>
+				<div class="col-md-8">
+					<div class="navmenu"style="text-align: center;">
+						<ul id="menu">
+							<li class="active" ><a href="#home">Home</a></li>
+							<li><a href="#about">About</a></li>
+							<li><a href="#project">Products</a></li>
+							<li class="last"><a href="#contact">Contact</a></li>
+							<!--li><a href="#features">Features</a></li-->
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+		<div class="container">
+			<div class="row wrap Ama" style="margin-top: 100px !important">
+				<div class="col-md-12 gallery">
+				  <h3>Welcome To</h3>
+				  <h3>Kamdhenu Borewell and Trading</h3>
+				  <h3>Kantabanji</h3>
+				  <input type="file" name="file_upload" id="file_upload" />
+				</div>
+			</div>
+		</div>
+	</div>
+    </div>
+    
+    <!--about start-->    
+    
+    <div id="about">
+    	<div class="line2">
+			<div class="container">
+				<div class="row Fresh">
+					<h1>About us</h1>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 wwa">
+					<span name="about" ></span>
+					<h3>We deals in Borewell Drilling, Solar Power, Pumping System and Column Pipes</h3>
+					<h3>The less you use, the more you save.</h3>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 centPic">
+					<img style="width: 35%" class="img-responsive"  src="images/save-water.jpg"/>
+				</div>
+			</div>	
+		</div>
+    </div>
+    <!--project start-->    
+    <div id="project">    	
+		<div class="line3">
+			<div class="container">
+				<div id="project1" ></div>
+				<div class="row Ama">
+					<div class="col-md-12">
+					<span name="projects" id="projectss"></span>
+					<h3>Our Products</h3>
+					</div>
+				</div>
+			</div>
+		</div>         
+       <div class="container">
+		<div class="row">
+			<div class="portfolio_block columns3   pretty" data-animated="fadeIn">	
+					<div class="element col-sm-4   gall branding">
+						<a class="plS" href="images/products/borewell.jpg" rel="prettyPhoto">
+							<img class="img-responsive picsGall" src="images/products/borewell.jpg" alt="pic1 Gallery"/>
+						</a>
+						<div class="view project_descr ">
+							<h3>Drilling Of Borewell</h3>
+						</div>
+					</div>
+					<div class="element col-sm-4  gall branding">
+						<a class="plS" href="images/products/solar.jpg" rel="prettyPhoto">
+							<img class="img-responsive picsGall"  src="images/products/solar.jpg" alt="pic2 Gallery"/>
+						</a>
+						<div class="view project_descr">
+							<h3>Solar Power</h3>
+						</div>
+					</div>
+					<div class="element col-sm-4  gall branding">
+						<a class="plS" href="images/products/pump.jpg" rel="prettyPhoto">
+							<img class="img-responsive picsGall" src="images/products/pump.jpg" alt="pic3 Gallery"/>
+						</a>
+						<div class="view project_descr ">
+							<h3>Pumping system</h3>
+						</div>
+					</div>
+					<div class="element col-sm-4   gall branding">
+						<a class="plS" href="images/products/pipe.jpg" rel="prettyPhoto">
+							<img class="img-responsive picsGall" src="images/products/pipe.jpg" alt="pic1 Gallery"/>
+						</a>
+						<div class="view project_descr">
+							<h3>Column Pipes</h3>
+						</div><br><br>
+					</div>				
+			</div>		
+		</div>
+		</div>
+    </div>    
+    <!--contact start-->
+    <div id="contact">
+    	<div class="line5">					
+			<div class="container">
+				<div class="row Ama">
+					<div class="col-md-12">
+					<h3>Contact Information</h3>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-offset-1 col-md-6 col-xs-12">
+					<address>
+					  <strong>Kamdhenu Borewell and Trading</strong><br>
+					  Near Om Prakash Jharwalia chowk<br>
+					  Bazar Pada<br>
+					  <strong>At/post:</strong> Kantabanji<br>
+					  <strong>Dist:</strong> Balangir (Odisha)<br>
+					  <strong>Pin:</strong> 767039<br>
+					  <abbr title="Phone">Contact no.:</abbr> 9938055444, 9937625834
+					</address>
+
+				</div>
+			</div>
+		</div>
+		<div class="line6">
+		  <script src='https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyACHsgBM02aXsn9z-gBEJoTTomuGHXsomM'>
+			
+		  </script>
+		  <div style='overflow:hidden;height:538px;width:1316px;'><div id='gmap_canvas' style='height:538px;width:1316px;'>
+			
+		  </div><style>#gmap_canvas img{max-width:none!important;background:none!important}</style></div>
+		  <a href='https://mapswebsite.org/'>map generator</a> <script type='text/javascript'
+		  src='https://embedmaps.com/google-maps-authorization/script.js?id=d31d84621b04ee5dd97e399be1c585432000e0d8'>
+			
+		  </script><script type='text/javascript'>function init_map(){var myOptions =
+		  {zoom:14,center:new google.maps.LatLng(20.4639112,82.92057369999998),mapTypeId: google.maps.MapTypeId.ROADMAP};map =
+		  new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(20.4639112,82.92057369999998)});infowindow = new google.maps.InfoWindow({content:'<strong>Kamdhenu Borewell and Trading</strong><br>Bazar Pada<br>767039 KANTABANJI<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script>
+		</div>
+		<div class="lineBlack">
+			<div class="container">
+				<div class="row downLine">
+					<div class="col-md-6 text-left copy">
+						<p>Copyright &copy; 2016 Kamdhenu Borewell and Trading. All Rights Reserved.</p>
+					</div>
+					<div class="col-md-6 text-right dm">
+						<ul id="downMenu">
+							<li class="active"><a href="#home">Home</a></li>
+							<li><a href="#about">About</a></li>
+							<li><a href="#project1">Products</a></li>
+							<li class="last"><a href="#contact">Contact</a></li>
+							<!--li><a href="#features">Features</a></li-->
+						</ul>
+					</div>
+				</div>
+				<div class="row">
+				  <div class="col-md-3">
+					  <p style="color: white">Website Design & Developed By:</p>							 
+					   <div style=" background-color :white !important">
+						 
+							 <a href="http://www.thewebfosters.com" target="_blank">
+								 <img src="images/logo.png" class="img-responsive" alt="The web fosters logo" style="width:100% !important">
+							 </a>
+						 					
+						 <p  class="text-center"  style = "color :#a94442 ; margin: 10px ">
+							<b>
+								  KANTABANJI <br/>
+								 +91 - 9437638555
+							</b>
+						</p>
+					 </div>							
+					</div>
+				</div>
+			</div>
+		</div>
+    </div>
+	
+	
+		
+		
+	<script src="js/jquery.prettyPhoto.js" type="text/javascript" charset="utf-8"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.slicknav.js"></script>
+	<script>
+			$(document).ready(function(){
+			$(".bhide").click(function(){
+				$(".hideObj").slideDown();
+				$(this).hide(); //.attr()
+				return false;
+			});
+			$(".bhide2").click(function(){
+				$(".container.hideObj2").slideDown();
+				$(this).hide(); // .attr()
+				return false;
+			});
+				
+			$('.heart').mouseover(function(){
+					$(this).find('i').removeClass('fa-heart-o').addClass('fa-heart');
+				}).mouseout(function(){
+					$(this).find('i').removeClass('fa-heart').addClass('fa-heart-o');
+				});
+				
+				function sdf_FTS(_number,_decimal,_separator)
+				{
+				var decimal=(typeof(_decimal)!='undefined')?_decimal:2;
+				var separator=(typeof(_separator)!='undefined')?_separator:'';
+				var r=parseFloat(_number)
+				var exp10=Math.pow(10,decimal);
+				r=Math.round(r*exp10)/exp10;
+				rr=Number(r).toFixed(decimal).toString().split('.');
+				b=rr[0].replace(/(\d{1,3}(?=(\d{3})+(?:\.\d|\b)))/g,"\$1"+separator);
+				r=(rr[1]?b+'.'+rr[1]:b);
+
+				return r;
+}
+				
+			setTimeout(function(){
+					$('#counter').text('0');
+					$('#counter1').text('0');
+					$('#counter2').text('0');
+					setInterval(function(){
+						
+						var curval=parseInt($('#counter').text());
+						var curval1=parseInt($('#counter1').text().replace(' ',''));
+						var curval2=parseInt($('#counter2').text());
+						if(curval<=707){
+							$('#counter').text(curval+1);
+						}
+						if(curval1<=12280){
+							$('#counter1').text(sdf_FTS((curval1+20),0,' '));
+						}
+						if(curval2<=245){
+							$('#counter2').text(curval2+1);
+						}
+					}, 2);
+					
+				}, 500);
+			});
+	</script>
+	<script type="text/javascript">
+	$(function() {
+		$('#file_upload').uploadify({
+			'swf'      : 'uploadify/uploadify.swf',
+			'uploader' : 'uploadify/uploadify.php',
+			'cancelImg' : 'uploadify/cancel.png',
+			'auto'      : true,
+			'buttonText' : 'Upload Photo',
+			'height' : 171 ,
+			'width' : 146
+			// Your options here
+		});
+	});
+	jQuery(document).ready(function(){
+		jQuery('#menu').slicknav();
+		
+	});
+	</script>
+	
+	<script type="text/javascript">
+    $(document).ready(function(){
+       
+        var $menu = $("#menuF");
+            
+        $(window).scroll(function(){
+            if ( $(this).scrollTop() > 100 && $menu.hasClass("default") ){
+                $menu.fadeOut('fast',function(){
+                    $(this).removeClass("default")
+                           .addClass("fixed transbg")
+                           .fadeIn('fast');
+                });
+            } else if($(this).scrollTop() <= 100 && $menu.hasClass("fixed")) {
+                $menu.fadeOut('fast',function(){
+                    $(this).removeClass("fixed transbg")
+                           .addClass("default")
+                           .fadeIn('fast');
+                });
+            }
+        });
+	});
+    //jQuery
+	  </script>
+	<script>
+		/*menu*/
+		function calculateScroll() {
+			var contentTop      =   [];
+			var contentBottom   =   [];
+			var winTop      =   $(window).scrollTop();
+			var rangeTop    =   200;
+			var rangeBottom =   500;
+			$('.navmenu').find('a').each(function(){
+				contentTop.push( $( $(this).attr('href') ).offset().top );
+				contentBottom.push( $( $(this).attr('href') ).offset().top + $( $(this).attr('href') ).height() );
+			})
+			$.each( contentTop, function(i){
+				if ( winTop > contentTop[i] - rangeTop && winTop < contentBottom[i] - rangeBottom ){
+					$('.navmenu li')
+					.removeClass('active')
+					.eq(i).addClass('active');				
+				}
+			})
+		};
+		
+		$(document).ready(function(){
+			calculateScroll();
+			$(window).scroll(function(event) {
+				calculateScroll();
+			});
+			$('.navmenu ul li a').click(function() {  
+				$('html, body').animate({scrollTop: $(this.hash).offset().top - 80}, 800);
+				return false;
+			});
+		});		
+	</script>	
+	<script type="text/javascript" charset="utf-8">
+
+		jQuery(document).ready(function(){
+			jQuery(".pretty a[rel^='prettyPhoto']").prettyPhoto({animation_speed:'normal',theme:'light_square',slideshow:3000, autoplay_slideshow: true, social_tools: ''});
+			
+		});
+	</script>
+	</body>
+	
+</html>
